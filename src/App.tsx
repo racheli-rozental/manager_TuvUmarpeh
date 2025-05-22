fb2ea86ad767295ceda820b761fc5af8b01ebc40
racheli-rozental <racheli74621@gmail.com>
2025-05-22 22:27:36 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
   import './App.css'
 // import Login from './components/Login'
-import UserNavigate from './components/UserNavigate'
-import { UserProvider } from './components/context'
+// import { UserProvider } from './components/context'
 import {RouterProvider } from 'react-router-dom'
 //import Navbar from './components/Navbar'
 import { router } from './router'
 // import Home from './components/Home'
 function App() {
   return (<>
-    <UserProvider>
-        <UserNavigate />
-        
-      
-        </UserProvider>
+   
             <RouterProvider router={router} />
   </>
   )
